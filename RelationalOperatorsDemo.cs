f52b75f3ea67575324aeebf7ee751c828f862a47
M.A.Miduna Varshini <mamidunavarshini369@gmail.com>
2026-01-07 06:35:44 +0530

    Create RelationalOperatorsDemo.cs

---
+++ RelationalOperatorsDemo.cs
@@ -0,0 +1,12 @@
+using System;
+class Example
+{
+    static void Main()
+    {
+        int a = 20;
+        int b = 20;
+        Console.WriteLine("a==b :" +(a==b));
+        Console.WriteLine("a>b : " + (a > b));
+        Console.WriteLine("a<b :" + (a < b));
+    }
+}
