8af925e029963d5353ba2b064b20473a104cb460
M.A.Miduna Varshini <mamidunavarshini369@gmail.com>
2026-01-07 06:28:10 +0530

    Create ArithmeticOperations.cs

---
+++ ArithmeticOperations.cs
@@ -0,0 +1,13 @@
+using System;
+class Example
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 20;
+        Console.WriteLine("Addition of a and b :" +(a + b));
+        Console.WriteLine("Subtraction of a and b :" + (a - b));
+        Console.WriteLine("Multiplication of a and b :" + (a * b));
+        Console.WriteLine("Division of a and b :" + (a / b));
+    }
+}
