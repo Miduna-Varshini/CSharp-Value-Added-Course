af8f3d89da199cdcb1659bc1c472df988bd5abc2
M.A.Miduna Varshini <mamidunavarshini369@gmail.com>
2026-01-07 06:21:39 +0530

    Create Student_Details.cs

---
+++ Student_Details.cs
@@ -0,0 +1,14 @@
+using System;
+class Example
+{
+    static void Main()
+    {
+        int studentID = 010;
+        string Studentname = "Ram";
+        string department = "Computer Science and Engineering";
+        Console.WriteLine("------STUDENT DETAILS ------- ");
+        Console.WriteLine ("Student ID :" +studentID);
+        Console.WriteLine("Student NAme :" +Studentname);
+        Console.WriteLine ("Department :" +department); 
+    }
+}
